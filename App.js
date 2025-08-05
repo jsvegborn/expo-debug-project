a9d6a43e36ae7027a1a30c305a2ea2ce0a27853a
Josefine Svegborn <josefine@knodd.se>
2025-08-05 13:31:40 +0200

    expo-video demo with subtitles

--- App.js
+++ App.js
@@ -1,11 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEvent } from "expo";
+import { VideoView, useVideoPlayer } from "expo-video";
+import { useEffect, useState } from "react";
+import { StyleSheet, View } from "react-native";
+
+const videoURI =
+  "https://stream.mux.com/39027PnoaVkt7qpHEHr9OHYSqDpyGPjYmnfDVHKkaUaY.m3u8";
 
 export default function App() {
+  const player = useVideoPlayer(videoURI, (initialPlayer) => {
+    initialPlayer.play();
+  });
+
+  const { availableSubtitleTracks } = useEvent(
+    player,
+    "availableSubtitleTracksChange",
+    { availableSubtitleTracks: player.availableSubtitleTracks }
+  );
+
+  useEffect(() => {
+    const swedishSubtitleTrack = availableSubtitleTracks.find(
+      (track) => track.language === "sv"
+    );
+
+    if (swedishSubtitleTrack) {
+      player.subtitleTrack = swedishSubtitleTrack;
+    } else {
+      player.subtitleTrack = null;
+    }
+  }, [availableSubtitleTracks, player]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <VideoView player={player} style={styles.video} contentFit={"contain"} />
     </View>
   );
 }
@@ -13,8 +39,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  video: {
+    width: "100%",
+    height: "100%",
   },
 });
